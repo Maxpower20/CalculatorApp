f6ffe80398afe86aa16c71ab638e3e1900f4a09f
Maxpower20 <maxymshpack@gmail.com>
2016-06-08 19:40:43 +0300

    Added log4j and aspectj dependencies

--- App/src/main/java/Bootstrap.java
+++ App/src/main/java/Bootstrap.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 
 public class Bootstrap {
     public static void main(String[] args) throws IOException {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml","aop-context.xml");
         Executor executor = applicationContext.getBean("executor", Executor.class);
 
         executor.execute();
-
     }
 }
